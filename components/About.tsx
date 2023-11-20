1efabc7b583bd33bbc1bcde677222e6c55213d1c
Lucas <lucasandrios@gmail.com>
2023-11-20 00:18:32 -0300

    scape '

--- components/About.tsx
+++ components/About.tsx
@@ -8,10 +8,10 @@ export const About = () => {
         <h2 className="lg:text-4xl font-semibold">What is Lorem Ipsum?</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
+          industry. Lorem Ipsum has been the industry(\')s standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled it to make a type specimen book. It has survived not
+          only five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
